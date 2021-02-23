17a6834396793a3e0e90226b1ce9297a98bf3e27
MURAT KAYA <kaya_murat59@hotmail.com>
2021-02-23 23:18:10 +0300

    WepAPI Eklendi

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -35,7 +35,7 @@ namespace Business.Concrete
         public IDataResult< List<Car>> GetAll()
         {
             // İş Kodları yazılır Buraya İş kodlarından geçtikten osnra veri erişim çağırmak gerek
-            if (DateTime.Now.Hour==22)
+            if (DateTime.Now.Hour==24)
             {
                 return new ErrorDataResult<List<Car>>(Messages.MaintenanceTime);
             }
